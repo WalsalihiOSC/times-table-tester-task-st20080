35f40a6427996a2a46769a816d67a5cfef0de2fb
Farhaan <91569964+st20080@users.noreply.github.com>
2022-04-12 17:15:15 +1200

    Update times_table.py

--- times_table.py
+++ times_table.py
@@ -19,12 +19,38 @@ class TimesTable:
 
         self.feedback = ttk.Label(parent, text = "Click 'Start' to begin!")
         self.feedback.grid(row = 0, column = 0)
-        
+
+
     def Next(self):
-        pass
+        self.feedback.configure(text = "",)
+        self.feedback.grid(row=1)
+        self.question.configure(text = "Question: ") 
+        num1 = random.randrange(1,10)
+        num2 = random.randrange(1,10)
+        display_question = "Question: {} {} {} = ".format(num1, "*", num2)
+        self.answer = num1*num2
+        self.question.configure(text = display_question)
+        self.check.grid(row = 1, column = 1)
+        self.input.grid(row = 0, column = 1)
+        self.next.grid_remove()
 
     def Check(self):
-        pass
+        try:
+            if int(self.input.get()) == int(self.answer):
+                self.input.delete(0,END)
+                self.input.focus()
+                self.Next()
+                self.feedback.config(text = "Correct")
+            else:
+                self.input.delete(0,END)
+                self.input.focus()
+                self.Next()
+                self.feedback.config(text = "Incorrect")
+
+        except ValueError:
+            self.feedback.configure(text = "Incorrect Input")
+            self.input.delete(0,END)
+            self.input.focus()
 
 if __name__ == "__main__":
     root = Tk()
