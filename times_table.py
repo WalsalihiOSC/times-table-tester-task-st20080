93a7598d2d5d269e8f6be9d0dd399893f03a8c83
Farhaan <91569964+st20080@users.noreply.github.com>
2022-04-12 15:44:16 +1200

    creating class

--- times_table.py
+++ times_table.py
@@ -2,3 +2,20 @@
 from tkinter import *
 from tkinter import ttk
 import random
+
+class TimesTable:
+
+    def __init__(self, parent):
+
+        self.question = ttk.Label(parent, text = "") 
+        self.question.grid(row = 0, column=0)
+
+        self.input = ttk.Entry(parent, width = 10)
+
+        self.check = ttk.Button(parent, text = "Check Answer")
+
+        self.next = ttk.Button(parent, text = "Start")
+        self.next.grid(row = 0, column = 1)
+
+        self.feedback = ttk.Label(parent, text = "Click 'Start' to begin!")
+        self.feedback.grid(row = 0, column = 0)
