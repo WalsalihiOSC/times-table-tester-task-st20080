1f010d9690250f3798ec1dd55431e1c7dfe9f48c
Farhaan <91569964+st20080@users.noreply.github.com>
2022-04-12 15:57:46 +1200

    Update times_table.py

--- times_table.py
+++ times_table.py
@@ -12,10 +12,22 @@ class TimesTable:
 
         self.input = ttk.Entry(parent, width = 10)
 
-        self.check = ttk.Button(parent, text = "Check Answer")
+        self.check = ttk.Button(parent, text = "Check Answer", command = self.Check)
 
-        self.next = ttk.Button(parent, text = "Start")
+        self.next = ttk.Button(parent, text = "Start", command = self.Next)
         self.next.grid(row = 0, column = 1)
 
         self.feedback = ttk.Label(parent, text = "Click 'Start' to begin!")
         self.feedback.grid(row = 0, column = 0)
+        
+    def Next(self):
+        pass
+
+    def Check(self):
+        pass
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Times Table")
+    TimesTable(root)
+    root.mainloop()
